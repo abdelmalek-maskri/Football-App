2daa2bfa1146084adc717e62f1689612cfa04b86
bsquire4 <bensquirep4@gmail.com>
2024-03-05 14:41:54 +0000

    user profile styling and removal of edit button on other user's profile

--- src/main/webapp/app/entities/user-profile/detail/user-profile-detail.component.ts
+++ src/main/webapp/app/entities/user-profile/detail/user-profile-detail.component.ts
@@ -3,19 +3,28 @@ import { ActivatedRoute } from '@angular/router';
 
 import { IUserProfile } from '../user-profile.model';
 import { DataUtils } from 'app/core/util/data-util.service';
+import { Account } from '../../../core/auth/account.model';
+import { AccountService } from '../../../core/auth/account.service';
 
 @Component({
   selector: 'jhi-user-profile-detail',
   templateUrl: './user-profile-detail.component.html',
+  styleUrls: ['./user-profile-detail.component.scss'],
 })
 export class UserProfileDetailComponent implements OnInit {
   userProfile: IUserProfile | null = null;
+  theAccount?: Account;
 
-  constructor(protected dataUtils: DataUtils, protected activatedRoute: ActivatedRoute) {}
+  constructor(protected dataUtils: DataUtils, protected activatedRoute: ActivatedRoute, private accountService: AccountService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ userProfile }) => {
       this.userProfile = userProfile;
+    });
+    this.accountService.getAuthenticationState().subscribe(account => {
+      if (account) {
+        this.theAccount = account;
+      }
     });
   }
 
